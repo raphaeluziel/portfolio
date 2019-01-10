cf8e14c51852efba93b1d03f672f50d6f4cceed3
Daniel Rosenwasser <DanielRosenwasser@users.noreply.github.com>
2019-01-09 22:28:06 -0800

    Avoid duplication for each project type.

--- js/portfolio.js
+++ js/portfolio.js
@@ -4,39 +4,22 @@ $(document).ready(function() {
     $('.all').show();
   });
 
-  $('#bootstrap').click(function(){
-    $('.all').hide();
-    $('.bootstrap').fadeIn(1000);
-  });
+  var projectTypes = [
+    'all',
+    'bootstrap',
+    'jQuery',
+    'react',
+    'd3',
+    'python',
+    'sql',
+    'flash'
+  ];
 
-  $('#jQuery').click(function(){
-    $('.all').hide();
-    $('.jQuery').fadeIn(1000);
-  });
-
-  $('#react').click(function(){
-    $('.all').hide();
-    $('.react').fadeIn(1000);
-  });
-
-  $('#d3').click(function(){
-    $('.all').hide();
-    $('.d3').fadeIn(1000);
-  });
-
-  $('#python').click(function(){
-    $('.all').hide();
-    $('.python').fadeIn(1000);
-  });
-
-  $('#sql').click(function(){
-    $('.all').hide();
-    $('.sql').fadeIn(1000);
-  });
-
-  $('#flash').click(function(){
-    $('.all').hide();
-    $('.flash').fadeIn(1000);
+  projectTypes.forEach(function (projectType) {
+    $('#' + projectType).click(function(){
+      $('.all').hide();
+      $('.' + projectType).fadeIn(1000);
+    });
   });
 
 });
